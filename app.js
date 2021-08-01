9b7bcfe7d599411e9234e7e48bb4ed7bda3134bd
ireti <79265428+iraytee-code@users.noreply.github.com>
2021-08-01 10:38:51 +0100

    commit on "main"

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-const toDoInput = document.getElementById("todoItem");
+const newTask = document.querySelector(".todo-button");
+
+newTask.addEventListener("click", addNewTask);
+
+function addNewTask(e){
+
+    e.preventDefault();
+
+    const newInput = document.getElementById("addTask");
+    
+}
