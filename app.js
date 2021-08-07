af2b3d91f431e51d587d7c407ef3f8527d56a6cd
iraytee-code <adeniranhope21@gmail.com>
2021-08-07 14:36:59 +0100

    Changes to be committed:

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const newTask = document.querySelector(".todo-button");
 
-newTask.addEventListener("click", addNewTask);
-
-function addNewTask(e){
-
-    e.preventDefault();
-
-    const newInput = document.getElementById("addTask");
+newTask.addEventListener("click", (event)=>{
     
-}
+    const newTask = document.querySelector(".task-input-1");
+
+    if(newTask.value === ""){
+        alert("Please enter a new TASK!");
+    }   else {
+
+        document.querySelector(".items").innerHTML += `
+
+            <label class = "list-item">
+            
+            <input type="checkbox" name="todoItem" id="todoItem" />
+                <p>
+                
+                ${
+                    document.querySelector('.task-input-1').value
+                }
+                
+                </p>
+                
+                <span></span>
+
+                <i class="fas fa-trash-alt"></i>
+            
+            </label>
+        
+        `;
+
+    }
+})
