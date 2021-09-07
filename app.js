c72a9d59a1018aebff441f9e86c497de80f9574f
Franklin U.O. Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2021-09-07 16:56:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 const todoButton = document.querySelector(".todo-button");
 const todoInput = document.querySelector(".todo-input");
+const todoContainer = document.querySelector(".todo-container");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener("click", () => {
   displayInputField();
-
-  let todoItem = document.createElement("li");
-  todoList.appendChild(todoItem);
-  todoList.innerHTML += `<li>
-  <label for="todoItem" class="execute"
-    ><input type="checkbox" name="todoItem" id="todoItem" />
-    <p class="text">${todoInput.value}</p>
-    <span></span>
-    <i class="fas fa-trash-alt"></i>
-  </label>
-</li>`;
-
-  const completedButton = document.querySelector(".execute");
-  completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.addEventListener("click", () => {
-    completedButton.classList.toggle("executed");
-  });
-
-  const deleteButton = document.querySelector(".fa-trash-alt");
-  deleteButton.innerHTML = '<i class="fas fa-delete"></i>';
-  deleteButton.addEventListener("click", () => {
-    deleteButton.removeChild(todoItem);
-  });
 });
 
 function displayInputField() {
   todoButton.classList.add("hide");
   todoInput.classList.remove("hide");
 }
+
+function addNewItem() {
+  todoInput.addEventListener("keydown", (e) => {
+    if (e.keyCode === 13) {
+      let todo = ` <li><label for="todoItem" class="execute">
+    <input type="checkbox" name="todoItem" id="todoItem" />
+    <p class="text">${todoInput.value}</p>
+    <span></span>
+    <i class="fas fa-trash-alt"></i>
+    </label></li>`;
+
+      todoList.insertAdjacentHTML("beforeend", todo);
+      todoList.appendChild(todo);
+    }
+    todoInput.value = "";
+
+    const deleteButton = document.querySelector(".fa-trash-alt");
+    deleteButton.addEventListener("click", () => {
+      deleteButton.parentNode.parentNode.remove(todoItem);
+    });
+
+    const completedButton = document.querySelector(".execute");
+    let enterItem = document.querySelector(".text");
+    enterItem.addEventListener("click", () => {
+      enterItem.classList.toggle("executed");
+    });
+  });
+}
+
+addNewItem();
